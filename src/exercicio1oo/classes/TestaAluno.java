e573c278479dd82b8fa58bdc342cdf67a2c24974
Herculano De Biasi <02105018980@udesc.br>
2025-09-01 20:47:03 -0300

    Exercício 1 e respectivo teste

---
+++ src/exercicio1oo/classes/TestaAluno.java
@@ -0,0 +1,22 @@
+package exercicio1oo.classes;
+
+public class TestaAluno {
+    public static void main(String[] args) {
+        Aluno herculano = new Aluno();
+        herculano.matricula = "CESMO-42";
+        herculano.nome = "Herculano De Biasi";
+        herculano.idade = 50;
+        herculano.nota1 = 8;
+        herculano.nota2 = 7;
+        herculano.nota3 = 9;
+        herculano.nota4 = 10;
+        System.out.println("Dados do Aluno");
+        System.out.println("Matrícula: " + herculano.matricula);
+        System.out.println("Nome.....: " + herculano.nome);
+        System.out.println("Idade....: " + herculano.idade);
+        System.out.println("Nota 1...: " + herculano.nota1);
+        System.out.println("Nota 2...: " + herculano.nota2);
+        System.out.println("Nota 3...: " + herculano.nota3);
+        System.out.println("Nota 4...: " + herculano.nota4);
+    }
+}
