2c07c012d52b1d773fdc46d5a87d00c4e559c38f
Herculano De Biasi <02105018980@udesc.br>
2025-09-01 21:09:13 -0300

    Classe Circulo e respectivo teste

---
+++ src/exercicio1oo/classes/TestaCirculo.java
@@ -0,0 +1,10 @@
+package exercicio1oo.classes;
+
+public class TestaCirculo {
+    public static void main(String[] args) {
+        Circulo bola = new Circulo();
+        bola.raio = 10;
+        System.out.println("Circulo raio");
+        System.out.println("Raio: " + bola.raio);
+    }
+}
